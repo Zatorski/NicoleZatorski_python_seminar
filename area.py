8d2a530c42c45784408d595886225c9fbf160f99
Nicole Zatorski <zatorski@mit.edu>
2015-02-03 16:58:40 -0500

    first file

---
+++ area.py
@@ -0,0 +1,11 @@
+#Title: Area of a Rectangle
+
+#These lines calculate the area of a rectangle.
+
+#Ingredients
+width = 20          #Length = meters
+height = 30         #Length = meters
+
+#Instructions
+area = width * height
+print area, 'meters'
