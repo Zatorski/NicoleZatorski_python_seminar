9d0080323a815f0d7a29bd0b7364e6c2b68d2b03
Zatorski <zatorski@mit.edu>
2015-02-24 15:00:11 -0500

    Homework 2 submission

---
+++ E&Mlecture2.py
@@ -0,0 +1,17 @@
+'''k = 1/(4piE)
+Electric Field = (kq)/r**2
+Gravitational Field = (Gm)/r**2
+Superposition is used to calculate the net electric field'''
+#floating point numbers- between integers
+##python does not store long bits on numerical data automaticaly
+##will show an L at the end to let you know it is not storing everything
+# 1- True
+# 0- False
+#None- the thing is completely empty
+#type() = tells you what the data type is: string, int, etc...
+#tuples = way to store several pieces of data as one unit
+#tuple[1] = 
+#alliasing
+'''looking for a file
+random.__file__
+will show you where the module is stored'''
---
+++ group_2.py
@@ -0,0 +1,3 @@
+# Lecture 2 Group Script
+# Point charages Field
+# group_2.py
